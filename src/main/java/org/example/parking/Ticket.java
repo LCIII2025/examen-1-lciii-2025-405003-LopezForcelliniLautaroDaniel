4944e91600e1081d30d087090870598262468933
405003-LopezForcelliniLautaroDaniel <405003@tecnicatura.frc.utn.edu.ar>
2025-05-22 20:07:25 -0300

    405003 lautaro daniel lopez forcellini parcial

--- src/main/java/org/example/parking/Ticket.java
+++ src/main/java/org/example/parking/Ticket.java
@@ -31,13 +31,24 @@ public class Ticket {
     }
 
     public double calcularPrecio() {
-        // TODO implementar el metodo para calcular el importe a abonar segun el tipo de vehiculo
-        // AUTO -> 100, SUV -> 130, PICKUP -> 180
-        // el importe es por hora redondeando el tiempo hacia arriba,
-        // por ejemplo si estuvo 45 minutos se le tarifa por 60, si estuvo 80 minutos se le tarifa por 120 minutos, etc...
-        // retornar el importe final
+        long minutos = calcularMinutos();
 
-        return 0;
+        long horas = (minutos + 59) / 60; 
+
+        double tarifaPorHora;
+        switch (vehiculo.getTipo()) {
+            case AUTO:
+                tarifaPorHora = 100;
+                break;
+            case SUV:
+                tarifaPorHora = 130;
+                break;
+           default:
+                tarifaPorHora = 180;
+                break;
+        }
+
+        return horas * tarifaPorHora;
     }
 
 }
--- src/test/java/org/example/parking/EstacionamientoTest.java
+++ src/test/java/org/example/parking/EstacionamientoTest.java
@@ -1,5 +1,8 @@
 package org.example.parking;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.example.parking.Vehiculo.Tipo;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -9,12 +12,21 @@ public class EstacionamientoTest {
 
     @Test
     public void testRetirarVehiculo() throws Exception {
-        //TODO test
+        Estacionamiento estacionamiento = new Estacionamiento();
+        Ticket ticket = estacionamiento.retirarVehiculo("4278");
+        assertNotNull(ticket);
     }
 
     @Test
     public void testCalcularPrecio() throws Exception {
-        // TODO test
+        Cliente cliente = new Cliente("42484023", "Lautaro");
+        Vehiculo vehiculo = new Vehiculo("4278", "2000", Tipo.AUTO);
+        Ticket ticket = new Ticket(cliente, vehiculo);
+        ticket.marcarSalida();
+
+        Double precio = ticket.calcularPrecio();
+        assertNotNull(precio);
+
     }
 
 }
